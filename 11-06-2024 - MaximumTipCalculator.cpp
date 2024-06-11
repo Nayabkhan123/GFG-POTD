3a4719493aef9fc172c3051c27c12ba52480a9f0
Nayabkhan123 <160863567+Nayabkhan123@users.noreply.github.com>
2024-06-12 01:45:13 +0530

    Add files via upload

---
+++ 11-06-2024 - MaximumTipCalculator.cpp
@@ -0,0 +1,31 @@
+long long maxTip(int n, int x, int y, vector<int> &arr, vector<int> &brr) {
+        vector<pair<int,int>>temp;
+        for(int i=0;i<n;i++) temp.push_back({abs(arr[i]-brr[i]),i});
+        long long ans=0;
+        sort(temp.begin(),temp.end());
+        for(int i=n-1;i>=0;i--){
+            int index=temp[i].second;
+            if(arr[index]>brr[index]){
+                if(x){
+                    ans+=arr[index];
+                    x--;
+                }
+                
+                else{
+                    ans+=brr[index];
+                    y--;
+                }
+            }
+            else{
+                if(y){
+                    ans+=brr[index];
+                    y--;
+                }
+                else{
+                    ans+=arr[index];
+                    x--;
+                }
+            }
+        }
+        return ans;
+    }
