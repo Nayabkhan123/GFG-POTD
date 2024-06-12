9757e65061191ea36b0183b879360b44b995eccb
Nayabkhan123 <160863567+Nayabkhan123@users.noreply.github.com>
2024-06-12 23:00:24 +0530

    Add files via upload

---
+++ 12-06-2024 - CountNumbersContaining4.cpp
@@ -0,0 +1,13 @@
+int countNumberswith4(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            string s = to_string(i);
+            for(auto it : s){
+                if(it=='4'){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
