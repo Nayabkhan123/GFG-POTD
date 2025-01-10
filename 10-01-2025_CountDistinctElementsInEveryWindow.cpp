f82fae90c76df5b04daa187bc62d6c6d93e4fb84
Nayab Khan <160863567+Nayabkhan123@users.noreply.github.com>
2025-01-10 17:36:48 +0530

    Add files via upload

---
+++ 10-01-2025_CountDistinctElementsInEveryWindow.cpp
@@ -0,0 +1,23 @@
+/*
+    Given an integer array arr[] and a number k. Find the count of distinct elements in every window of size k 
+    in the array.
+*/
+
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+            mp[arr[i]]++;
+        }
+        ans.push_back(mp.size());
+        for(int i=k;i<arr.size();i++){
+            mp[arr[i]]++;
+            mp[arr[i-k]]--;
+            if(mp[arr[i-k]]==0) mp.erase(arr[i-k]);
+            ans.push_back(mp.size());
+        }
+        return ans;
+    }
+};
